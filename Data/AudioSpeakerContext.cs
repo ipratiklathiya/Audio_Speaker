86892d5dd4eb0bc34f325ff3fe2095759d661e50
ipratiklathiya <w0791684@myscc.ca>
2022-10-14 03:17:04 -0400

    Commit till the database class file

---
+++ Data/AudioSpeakerContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Audio_Speaker.Models;
+
+namespace Audio_Speaker.Data
+{
+ 
+        public class AudioSpeakerContext : DbContext
+        {
+            public AudioSpeakerContext(DbContextOptions<AudioSpeakerContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Speaker> Speaker { get; set; }
+        }
+    }
+}
